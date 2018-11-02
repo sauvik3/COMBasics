40196bddcd51edabf25d12fd444ecc04ae6353ff
sauvik3 <sauvik3@gmail.com>
2018-11-02 17:56:59 +0530

    Update CalculationObj.cpp

--- CalcCOMObject/CalculationObj.cpp
+++ CalcCOMObject/CalculationObj.cpp
@@ -1,32 +1,30 @@
 #include "CalculationObj.h"
 #include "Calculation_i.c"
 
-HRESULT CCalculationObj::QueryInterface(REFIID riid, void ** ppObj) {
-	*ppObj = NULL;
-	if (riid == IID_IUnknown) {
-		*ppObj = static_cast<void*>(this);
-		AddRef();
-		return S_OK;
-	}
-	if (riid == IID_ICalculation) {
-		*ppObj = static_cast<void*>(this);
-		AddRef();
-		return S_OK;
-	}
-	return E_NOINTERFACE;
+HRESULT CCalculationObj::QueryInterface(REFIID riid, LPVOID *ppObj) {
+    if ((riid == IID_IUnknown) || (riid == IID_ICalculation)) {
+        *ppObj = static_cast<ICalculation *>(this);
+    }
+    else {
+        *ppObj = NULL;
+        return E_NOINTERFACE;
+    }
+    reinterpret_cast<IUnknown*>(*ppObj)->AddRef();
+    return S_OK;
 }
 
 ULONG CCalculationObj::AddRef()
 {
-	return InterlockedIncrement(&m_nRefCount);
+	return ::InterlockedIncrement(&m_nRefCount);
 }
 
 ULONG CCalculationObj::Release()
 {
-	long nRefCount = InterlockedDecrement(&m_nRefCount);
-	if (nRefCount == 0)
-		delete this;
-	return nRefCount;
+    if (::InterlockedDecrement(&m_nRefCount) == 0) {
+        delete this;
+        return 0;
+    }
+    return m_nRefCount;
 }
 
 HRESULT CCalculationObj::Addition(int op1, int op2, int * result)
