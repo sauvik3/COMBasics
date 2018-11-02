1de051d781ee525076f06b81fa83dc4f4fdd15f4
sauvik3 <sauvik3@gmail.com>
2018-11-02 13:57:44 +0530

    Add error handler.

--- CalcCOMObjectTest/Client.cpp
+++ CalcCOMObjectTest/Client.cpp
@@ -1,10 +1,6 @@
 #include <objbase.h>
-#include <stdio.h>
+#include <iostream>
 #import "CalcCOMObject.dll"
-//
-//Here we do a #import on the DLL ,you can also do a #import on the .TLB 
-//The #import directive generates two files (.tlh/.tli) in the output folders.
-//
 
 int main()
 {
@@ -12,20 +8,19 @@ int main()
 	int n1 = 100, n2 = 200;
 	int nOutPut = 0;
 
-	CoInitialize(NULL);
-	MyMathLib::ICalculationPtr pCalculationObj;
-	//
-	//ICalculationPtr is not the actual interface ICalculation, but a template C++ class (_com_ptr_t)
-	//that contains an embedded instance of the raw ICalculation pointer
-	//While destructing , the destructor makes sure to invoke Release() on the internal
-	//raw interface pointer. Further, the operator -> has been overloaded to direct all
-	//method invocations to the internal raw interface pointer.
-	//
-	pCalculationObj.CreateInstance("MyCOMLibs.Calculation");
-
-	pCalculationObj->Addition(n1, n2, &nOutPut);//"->" overloading in action
-
-	printf("\nOutput after adding %d & %d is %d\n", n1, n2, nOutPut);
+    try
+    {
+        CoInitialize(NULL);
+        MyMathLib::ICalculationPtr pCalculationObj;
+        HRESULT hr = pCalculationObj.CreateInstance("MyCOMLibs.Calculation");
+        _com_util::CheckError(hr);
+        pCalculationObj->Addition(n1, n2, &nOutPut);
+        std::cout << std::endl << "Output after adding " << n1 << " and " << n2 << " is " << nOutPut << ".";
+    }
+    catch (const _com_error& e)
+    {
+        std::wcerr << L"Error : " << e.ErrorMessage();
+    }
 
 	return 0;
 }
