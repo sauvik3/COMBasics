265f913d114ef1f94397a65e505c4e0a16348f78
sauvik3 <sauvik3@gmail.com>
2020-08-30 21:50:14 +0530

    Update Unit Test file

--- UnitTests/unittests.cpp
+++ UnitTests/unittests.cpp
@@ -1,8 +1,47 @@
 #include "CppUnitTestLogger.h"
 #include "CalculationObj.h"
 #include "unittests.h"
+#include <comutil.h>
+#include <comdef.h>
+#include <Tchar.h>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+void UnitTests::CCalculationObjTest::InitTests()
+{
+	Logger::WriteMessage("Initializing COM Library...");
+	try
+	{
+		CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+		const HRESULT hr = CoCreateInstance(CLSID_CalcObject, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pCalculationObj));
+		_com_util::CheckError(hr);
+	}
+	catch (const _com_error& e)
+	{
+		const size_t string_size = 80;
+		TCHAR value[string_size] = _T("COM Error: ");
+		_tcscat_s(value, string_size, e.ErrorMessage());
+		Logger::WriteMessage(value);
+	}
+	Assert::IsNotNull(pCalculationObj);
+}
+
+void UnitTests::CCalculationObjTest::CleanupTests()
+{
+	Logger::WriteMessage("Releasing COM Library...");
+	try
+	{
+		Assert::IsNotNull(pCalculationObj);
+		pCalculationObj->Release();
+	}
+	catch (const _com_error& e)
+	{
+		const size_t string_size = 80;
+		TCHAR value[string_size] = _T("COM Error: ");
+		_tcscat_s(value, string_size, e.ErrorMessage());
+		Logger::WriteMessage(value);
+	}
+}
 
 void UnitTests::CCalculationObjTest::CCalculationObj_AdditionTest()
 {
@@ -11,8 +50,7 @@ void UnitTests::CCalculationObjTest::CCalculationObj_AdditionTest()
 	int nOutPut = 0;
 	try
 	{
-		CCalculationObj calcObj;
-		calcObj.Addition(n1, n2, &nOutPut);
+		pCalculationObj->Addition(n1, n2, &nOutPut);
 		Assert::AreEqual(nOutPut, 300);
 	}
 	catch (const std::exception& e)
@@ -28,8 +66,7 @@ void UnitTests::CCalculationObjTest::CCalculationObj_SubtractionTest()
 	int nOutPut = 0;
 	try
 	{
-		CCalculationObj calcObj;
-		calcObj.Subtraction(n1, n2, &nOutPut);
+		pCalculationObj->Subtraction(n1, n2, &nOutPut);
 		Assert::AreEqual(nOutPut, 300);
 	}
 	catch (const std::exception& e)
--- UnitTests/unittests.h
+++ UnitTests/unittests.h
@@ -9,7 +9,12 @@ namespace UnitTests
 	TEST_CLASS(CCalculationObjTest)
 	{
 	public:
+		TEST_METHOD_INITIALIZE(InitTests);
 		TEST_METHOD(CCalculationObj_AdditionTest);
 		TEST_METHOD(CCalculationObj_SubtractionTest);
+		TEST_METHOD_CLEANUP(CleanupTests);
+
+	private:
+		ICalculation* pCalculationObj;
 	};
 }
